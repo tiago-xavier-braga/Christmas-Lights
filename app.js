ea1d31cb16f573c328cf99698bc427a3611039d9
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-08-16 15:29:26 -0300

    Modified fuction for modify css

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var light = document.getElementById('light');
+var baseLamp = 
+` <div class="lamp">
+        <div class="mouth">
+        </div>
+        <div class="light" id="light">
+        </div>
+</div>`
+var box = document.getElementsByClassName('box_vertical');
+box.innerHTML = baseLamp;
+
 
 function turnOn() {
-        light.className = light.className.concat(' on')
+        const sheet = new CSSStyleSheet();
+        sheet.insertRule(`.on {animation: onLight ${document.getElementById('speed').value}s alternate infinite ease-in-out;}`);
+        document.adoptedStyleSheets = [sheet];
+
+        light.className = light.className.concat(' on');;
+}
+function turnOff(){
+        light.className = 'light';
 }

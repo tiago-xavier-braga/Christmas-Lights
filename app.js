b44d1e01a8e97f07d2ec14acef13b9b9d615d89d
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-08-19 19:58:34 -0300

    Completed script

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-var light = document.getElementById('light');
 var box = document.getElementById('box');
 const colors = ['#c0392b', '#f1c40f', '#64fcfe', '#2ecc71' ];
 
-for(let i = 0; i < 5; i++){
-        var html = `
-        <div class="lamp">
-                <div class="mouth">
-                </div>
-                <div class="light" id="light" style="background-color: ${colors[i]}">
-                </div>
-        </div>
-        `;
-        box.innerHTML += html;
-}
-
 function turnOn() {
+        box.innerHTML= '';
+        for(let i = 0; i < 5; i++){
+                var html = `
+                <div class="lamp">
+                        <div class="mouth">
+                        </div>
+                        <div class="light on" id="light" style="background-color: ${colors[i]}">
+                        </div>
+                </div>
+                `;
+                box.innerHTML += html;
+        }
+
         const sheet = new CSSStyleSheet();
         sheet.insertRule(`.on {animation: onLight ${document.getElementById('speed').value}s alternate infinite ease-in-out;}`);
-        /*
         document.adoptedStyleSheets = [sheet];
 
-        light.className = 'light on';
-        */
-        const classeQueDesejoAlterar = ".italic";
-
-        /* Percorre todas as regras CSS do primeiro Stylesheet da página */
-        let cssRuleList = [...document.styleSheets[0].cssRules].filter( rule => rule.selectorText == classeQueDesejoAlterar)
-        
-        /* Percorre todas as regras capturadas e aplica o CSS */
-        for (let cssRule of cssRuleList) {
-          cssRule.style.setProperty("background", "red")
-        }
+        document.getElementById('light').className = 'light on';
 }
 function turnOff(){
-        light.className = 'light';
+        document.getElementById('light').className = 'light';
 }
